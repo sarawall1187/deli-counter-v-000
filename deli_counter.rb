50d5d4b300cbf6afc53f523b8320098ed5b37c4e
Sara Walker <sarawall1187@gmail.com>
2018-12-13 18:35:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli) 
+  if katz_deli == []
+    puts "The line is currently empty."
+  elsif katz_deli.map.with_index { |person, index| 
+   puts "The line is currently: #{person[index]}."}
+ end
+end
+
+
+
+# katz_deli,map.with_index{ |person, index| #something to do here }
+# # line line method- takes in argument of katz_deli
+# if the line is empty, put line is empty
+# Add and subtract customers from line and place them in numbered order
